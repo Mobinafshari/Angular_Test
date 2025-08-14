6a0863e797c4c9735e752ffa71b2f3810ae48164
Mobinafshari <mma137813781378@gmail.com>
2025-08-14 15:34:10 +0330

    fix: trying to connect components to each other

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,11 +1,13 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { ChildComponent } from './pages/child';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, ChildComponent],
   templateUrl: './app.html',
-  styleUrl: './app.scss'
+  styleUrl: './app.scss',
+  standalone: true,
 })
 export class App {
   protected readonly title = signal('Angular-Test');
