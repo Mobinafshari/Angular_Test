f0f4173468c93808d27ef8e87b6e329b272326b2
Mobinafshari <mma137813781378@gmail.com>
2025-08-14 16:14:14 +0330

    fix: testing some stuffs

--- src/app/pages/child.ts
+++ src/app/pages/child.ts
@@ -3,8 +3,31 @@ import { Component, Input } from '@angular/core';
 @Component({
   selector: 'app-child',
   standalone: true,
-  template: `<p>Child says: {{ message }}</p>`,
+  template: `<p>Child says: {{ message }}</p>
+    <input type="text" placeholder="enter your name here" (input)="handleInputChange($event)" />
+    @if(isAdmin){
+    <p>You are Admin</p>
+    }@else{
+    <p>Normal User = {{ name }}</p>
+    }
+    <button (click)="handleClick()">I am not admin</button>`,
 })
 export class ChildComponent {
   @Input() message = '';
+  name = '';
+  age = 0;
+  isAdmin = true;
+  updateInfos({ name, age }: { name: string; age: number }) {
+    this.name = name;
+    this.age = age;
+  }
+  updateName(name: string) {
+    this.name = name;
+  }
+  handleClick() {
+    this.isAdmin = false;
+  }
+  handleInputChange(e: any) {
+    this.updateName(e.target.value);
+  }
 }
