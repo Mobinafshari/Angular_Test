4d1203c0c1a03fb98d6e3dd73a6e006e2170c301
Mobinafshari <mma137813781378@gmail.com>
2025-08-18 22:41:43 +0330

    fix: older files removed and started from scratch for new angular project

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,10 +1,9 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { ChildComponent } from './pages/child';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, ChildComponent],
+  imports: [RouterOutlet],
   templateUrl: './app.html',
   styleUrl: './app.scss',
   standalone: true,
