3e2e45dcb9ddbdd3daeb1a13dd9a2d53de12e2fb
Mobinafshari <mma137813781378@gmail.com>
2025-08-20 23:25:58 +0330

    fix: used signal and last changes

--- src/app/user/user.ts
+++ src/app/user/user.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, computed, signal } from '@angular/core';
 import { USERS_LIST } from '../users-list';
 
 const randomIndex = Math.floor(Math.random() * USERS_LIST.length);
@@ -9,5 +9,13 @@ const randomIndex = Math.floor(Math.random() * USERS_LIST.length);
   styleUrl: './user.scss',
 })
 export class User {
-  selectedUser = USERS_LIST[randomIndex];
+  selectedUser = signal(USERS_LIST[randomIndex]);
+  imagePath = computed(() => 'assets/users/' + this.selectedUser().image);
+  // get imagePath() {
+  //   return 'assets/users/' + this.selectedUser.image;
+  // }
+  onSelectUser() {
+    const randomIndex = Math.floor(Math.random() * USERS_LIST.length);
+    this.selectedUser.set(USERS_LIST[randomIndex]);
+  }
 }
