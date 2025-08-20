c32aa850782519e72c72eb92f8e8c0efc9a755b1
Mobinafshari <mma137813781378@gmail.com>
2025-08-20 22:17:39 +0330

    fix: working on stuff

--- src/app/user/user.ts
+++ src/app/user/user.ts
@@ -1,11 +1,13 @@
 import { Component } from '@angular/core';
+import { USERS_LIST } from '../users-list';
 
+const randomIndex = Math.floor(Math.random() * USERS_LIST.length);
 @Component({
   selector: 'app-user',
   imports: [],
   templateUrl: './user.html',
-  styleUrl: './user.scss'
+  styleUrl: './user.scss',
 })
 export class User {
-
+  selectedUser = USERS_LIST[randomIndex];
 }
---
+++ src/app/users-list.ts
@@ -0,0 +1,32 @@
+export const USERS_LIST = [
+  {
+    id: 1,
+    name: 'hashem',
+    image: 'user-1.jpg',
+  },
+  {
+    id: 2,
+    name: 'hashem2',
+    image: 'user-2.jpg',
+  },
+  {
+    id: 3,
+    name: 'hashem3',
+    image: 'user-3.jpg',
+  },
+  {
+    id: 4,
+    name: 'hashem4',
+    image: 'user-4.jpg',
+  },
+  {
+    id: 5,
+    name: 'hashem5',
+    image: 'user-5.jpg',
+  },
+  {
+    id: 6,
+    name: 'hashem6',
+    image: 'user-6.jpg',
+  },
+];
