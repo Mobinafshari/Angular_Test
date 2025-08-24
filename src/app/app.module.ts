03da67204bd595b1ae4059d92028075adfbb7f26
Mobinafshari <mma137813781378@gmail.com>
2025-08-24 13:00:04 +0330

    fix: modules was shitty

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { App } from './app';
+import { HeaderComponent } from './header/header';
+import { User } from './user/user';
+import { UserTask } from './user-task/user-task';
+import { BrowserModule } from '@angular/platform-browser';
+
 @NgModule({
   declarations: [App],
   bootstrap: [App],
+  imports: [BrowserModule, HeaderComponent, User, UserTask],
 })
 export class AppModule {}
