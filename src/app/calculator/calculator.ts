86590f23ad7aea9ea4333751150dcfe43795ebcb
Mobinafshari <mma137813781378@gmail.com>
2025-08-24 17:50:45 +0330

    fix: fixed input component for outer access

--- src/app/calculator/calculator.ts
+++ src/app/calculator/calculator.ts
@@ -1,12 +1,19 @@
 import { Component } from '@angular/core';
-import { InputComponent } from "../shared/input/input";
+import { InputComponent } from '../shared/input/input';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-calculator',
-  imports: [InputComponent],
+  imports: [InputComponent, FormsModule],
   templateUrl: './calculator.html',
-  styleUrl: './calculator.scss'
+  styleUrl: './calculator.scss',
 })
 export class Calculator {
-
+  initial = '';
+  annual = '';
+  expected = '';
+  duration = '';
+  handleForm() {
+    console.log(this.annual, this.initial, this.expected, this.duration);
+  }
 }
--- src/app/shared/input/input.ts
+++ src/app/shared/input/input.ts
@@ -1,11 +1,40 @@
-import { Component, Input } from '@angular/core';
+import { Component, forwardRef, Input } from '@angular/core';
+import { ControlValueAccessor, NG_VALUE_ACCESSOR } from '@angular/forms';
 
 @Component({
   selector: 'app-input',
   imports: [],
   templateUrl: './input.html',
+  providers: [
+    {
+      provide: NG_VALUE_ACCESSOR,
+      useExisting: forwardRef(() => InputComponent),
+      multi: true,
+    },
+  ],
   styleUrl: './input.scss',
 })
-export class InputComponent {
+export class InputComponent implements ControlValueAccessor {
   @Input() type? = 'number';
+  value: any = '';
+
+  private onChange: any = () => {};
+  private onTouched: any = () => {};
+
+  onInput(event: Event) {
+    const input = event.target as HTMLInputElement;
+    this.value = input.value;
+    this.onChange(this.value);
+    this.onTouched();
+  }
+
+  writeValue(value: any): void {
+    this.value = value ?? '';
+  }
+  registerOnChange(fn: any): void {
+    this.onChange = fn;
+  }
+  registerOnTouched(fn: any): void {
+    this.onTouched = fn;
+  }
 }
