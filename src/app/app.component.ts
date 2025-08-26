edabd740af991a108d0d5cd605e3699e219c603a
Mobinafshari <mma137813781378@gmail.com>
2025-08-26 21:48:44 +0330

    feat: added a wrapper for dashboard components

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,12 +3,13 @@ import { Header } from "./header/header";
 import { ServerStatus } from "./dashboard/server-status/server-status";
 import { Traffic } from "./dashboard/traffic/traffic";
 import { Ticket } from "./dashboard/ticket/ticket";
+import { DashboardItem } from "./dashboard/dashboard-item/dashboard-item";
 
 @Component({
   selector: 'app-root',
   standalone: true,
   templateUrl: './app.component.html',
-  imports: [Header, ServerStatus, Traffic, Ticket],
+  imports: [Header, ServerStatus, Traffic, Ticket, DashboardItem],
 })
 export class AppComponent {
 
