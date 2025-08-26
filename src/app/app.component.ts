6fca4ad64dab601e3335392cca14e8a98262f1ff
Mobinafshari <mma137813781378@gmail.com>
2025-08-26 22:05:15 +0330

    feat: added some components related to the tickets

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,16 +1,14 @@
 import { Component } from '@angular/core';
-import { Header } from "./header/header";
-import { ServerStatus } from "./dashboard/server-status/server-status";
-import { Traffic } from "./dashboard/traffic/traffic";
-import { Ticket } from "./dashboard/ticket/ticket";
-import { DashboardItem } from "./dashboard/dashboard-item/dashboard-item";
+import { Header } from './header/header';
+import { ServerStatus } from './dashboard/server-status/server-status';
+import { Traffic } from './dashboard/traffic/traffic';
+import { Tickets } from './dashboard/tickets/tickets';
+import { DashboardItem } from './dashboard/dashboard-item/dashboard-item';
 
 @Component({
   selector: 'app-root',
   standalone: true,
   templateUrl: './app.component.html',
-  imports: [Header, ServerStatus, Traffic, Ticket, DashboardItem],
+  imports: [Header, ServerStatus, Traffic, Tickets, DashboardItem],
 })
-export class AppComponent {
-
-}
+export class AppComponent {}
