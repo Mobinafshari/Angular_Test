22937c020493ae5de6537a1aae73c71561f6897a
Mobinafshari <mma137813781378@gmail.com>
2025-08-29 16:07:55 +0330

    feat: working with angular life cycle

--- src/app/dashboard/server-status/server-status.ts
+++ src/app/dashboard/server-status/server-status.ts
@@ -1,5 +1,11 @@
 import { Component } from '@angular/core';
 
+enum ServerStatusEnum {
+  ONLINE = 'online',
+  OFFLINE = 'offline',
+  UNKNOWN = 'unknown',
+}
+
 @Component({
   selector: 'app-server-status',
   imports: [],
@@ -7,5 +13,18 @@ import { Component } from '@angular/core';
   styleUrl: './server-status.scss',
 })
 export class ServerStatus {
-  currentStatus = 'offline';
+  currentStatus = ServerStatusEnum.ONLINE;
+
+  ngOnInit() {
+    setInterval(() => {
+      const rnd = Math.random();
+      if (rnd < 0.5) {
+        this.currentStatus = ServerStatusEnum.ONLINE;
+      } else if (rnd < 0.9) {
+        this.currentStatus = ServerStatusEnum.OFFLINE;
+      } else {
+        this.currentStatus = ServerStatusEnum.UNKNOWN;
+      }
+    }, 3000);
+  }
 }
