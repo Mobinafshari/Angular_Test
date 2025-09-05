99a8402d07144e5e0d754ac23a21d9eaf174ed49
Mobinafshari <mma137813781378@gmail.com>
2025-09-05 14:58:27 +0330

    feat: working on another project from scratch

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,5 @@
 import { bootstrapApplication } from '@angular/platform-browser';
 
 import { AppComponent } from './app/app.component';
-import { TaskService } from './app/tasks/tasks.service';
 
 bootstrapApplication(AppComponent).catch((err) => console.error(err));
