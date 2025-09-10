731c73c66ec4be8eaea45372f3c5493f2db4ee5d
Mobin Afshari <mma137813781378@gmail.com>
2025-09-10 12:07:56 +0330

    feat: starting another project from scratch and working on http requests

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,41 +1,13 @@
-import { Component, DestroyRef, inject, OnInit, signal } from '@angular/core';
-import { toObservable } from '@angular/core/rxjs-interop';
-import { interval, map, Observable } from 'rxjs';
+import { Component } from '@angular/core';
+
+import { AvailablePlacesComponent } from './places/available-places/available-places.component';
+import { UserPlacesComponent } from './places/user-places/user-places.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
   templateUrl: './app.component.html',
+  styleUrl: './app.component.css',
+  imports: [AvailablePlacesComponent, UserPlacesComponent],
 })
-export class AppComponent implements OnInit {
-  clickCount = signal(0);
-  clickCount$ = toObservable(this.clickCount);
-  private destroyRef = inject(DestroyRef);
-  constructor() {}
-  private customInterval$ = new Observable((subscriber) => {
-    let count = 0;
-    const interval = setInterval(() => {
-      if (count > 3) {
-        clearInterval(interval);
-        subscriber.complete();
-      }
-      subscriber.next({ message: 'Hello Darkness!' });
-      count++;
-    }, 1000);
-  });
-  ngOnInit(): void {
-    // const subscription = interval(1000)
-    //   .pipe(map((val) => val * 2))
-    //   .subscribe({
-    //     next: (value) => console.log(value),
-    //   });
-    // this.destroyRef.onDestroy(() => {
-    //   subscription.unsubscribe();
-    // });
-    const subscription = this.clickCount$.subscribe({
-      next: (val) => console.log(`${val} clicked`),
-    });
-    this.destroyRef.onDestroy(() => subscription.unsubscribe());
-  }
-  onClick() {}
-}
+export class AppComponent {}
