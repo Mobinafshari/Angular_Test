a5bc88dea71d0013c8401d171688c2f270b259a9
Mobin Afshari <mma137813781378@gmail.com>
2025-09-12 19:35:48 +0330

    feat: trying to add template-driven way to handle forms

--- src/app/auth/login/login.component.ts
+++ src/app/auth/login/login.component.ts
@@ -1,9 +1,16 @@
 import { Component } from '@angular/core';
+import { FormsModule, NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
   standalone: true,
   templateUrl: './login.component.html',
   styleUrl: './login.component.css',
+  imports: [FormsModule],
 })
-export class LoginComponent {}
+export class LoginComponent {
+  handleForm(formData: NgForm) {
+    if (!formData.form.valid) return;
+    console.log(formData.form.value);
+  }
+}
