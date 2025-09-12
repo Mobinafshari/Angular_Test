bad55729a7afcd7f77061494eb94f5eed97718e1
Mobin Afshari <mma137813781378@gmail.com>
2025-09-12 19:10:59 +0330

    feat: working on a project from scratch for handling forms

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,5 @@
 import { bootstrapApplication } from '@angular/platform-browser';
 
 import { AppComponent } from './app/app.component';
-import { provideHttpClient, withInterceptors } from '@angular/common/http';
-import { loggingInterceptor } from './app/service/loggingInterceptor';
 
-bootstrapApplication(AppComponent, {
-  providers: [provideHttpClient(withInterceptors([loggingInterceptor]))],
-}).catch((err) => console.error(err));
+bootstrapApplication(AppComponent).catch((err) => console.error(err));
