f4f4944b4d57d0855f2b957cd2f3552098356ef8
Mobin Afshari <mma137813781378@gmail.com>
2025-09-14 23:11:57 +0330

    fix: last changes till tomorrow

--- src/app/auth/login/login.component.ts
+++ src/app/auth/login/login.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, DestroyRef, inject, OnInit } from '@angular/core';
 import {
   AbstractControl,
   FormControl,
@@ -6,12 +6,26 @@ import {
   ReactiveFormsModule,
   Validators,
 } from '@angular/forms';
+import { debounceTime, of } from 'rxjs';
 
 function mustContainQuestionMark(control: AbstractControl) {
   if (control.value.includes('?')) return null;
   return {
     doesNotContainQuestionMark: true,
   };
+}
+
+function uniqueEmail(control: AbstractControl) {
+  if (control.value.email !== 'test@gmail.com') {
+    return of(null);
+  }
+  return of({ uniqueEmailError: true });
+}
+let initialMail = '';
+const existedEmail = localStorage.getItem('email');
+if (existedEmail) {
+  const savedEmail = JSON.parse(existedEmail);
+  initialMail = savedEmail;
 }
 
 @Component({
@@ -21,15 +35,30 @@ function mustContainQuestionMark(control: AbstractControl) {
   styleUrl: './login.component.css',
   imports: [ReactiveFormsModule],
 })
-export class LoginComponent {
+export class LoginComponent implements OnInit {
+  private destroyRef = inject(DestroyRef);
   form = new FormGroup({
-    email: new FormControl('', {
+    email: new FormControl(initialMail, {
       validators: [Validators.email, Validators.required],
+      asyncValidators: uniqueEmail,
     }),
     password: new FormControl('', {
       validators: [Validators.required, Validators.minLength(6), mustContainQuestionMark],
     }),
   });
+  ngOnInit(): void {
+    // const existedEmail = localStorage.getItem('email');
+    // if (existedEmail) {
+    //   const savedEmail = JSON.parse(existedEmail);
+    //   this.form.patchValue({ email: savedEmail });
+    // }
+
+    const subscription = this.form.valueChanges.pipe(debounceTime(500)).subscribe({
+      next: (value) => localStorage.setItem('email', JSON.stringify({ email: value.email })),
+    });
+    this.destroyRef.onDestroy(() => subscription.unsubscribe());
+  }
+
   get EmailError() {
     return (
       this.form.controls.email.touched &&
