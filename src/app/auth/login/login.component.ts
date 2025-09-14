da57ac3feeb542f88189c7302707d12924aa782b
Mobin Afshari <mma137813781378@gmail.com>
2025-09-14 21:04:32 +0330

    fix: working with template driven forms features

--- src/app/auth/login/login.component.ts
+++ src/app/auth/login/login.component.ts
@@ -1,5 +1,6 @@
-import { Component } from '@angular/core';
+import { afterNextRender, Component, DestroyRef, inject, viewChild } from '@angular/core';
 import { FormsModule, NgForm } from '@angular/forms';
+import { debounceTime } from 'rxjs';
 
 @Component({
   selector: 'app-login',
@@ -9,8 +10,28 @@ import { FormsModule, NgForm } from '@angular/forms';
   imports: [FormsModule],
 })
 export class LoginComponent {
+  private form = viewChild<NgForm>('form');
+  private destroyRef = inject(DestroyRef);
+  constructor() {
+    const savedEmail = localStorage.getItem('email');
+    if (savedEmail) {
+      this.form()?.setValue({
+        email: savedEmail,
+        password: '',
+      });
+    }
+    afterNextRender(() => {
+      const subscription = this.form()
+        ?.valueChanges?.pipe(debounceTime(500))
+        .subscribe({
+          next: (value) => localStorage.setItem('email', value.email),
+        });
+      this.destroyRef.onDestroy(() => subscription?.unsubscribe());
+    });
+  }
   handleForm(formData: NgForm) {
     if (!formData.form.valid) return;
     console.log(formData.form.value);
+    formData.form.reset();
   }
 }
