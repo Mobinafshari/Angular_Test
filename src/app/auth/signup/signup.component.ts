f8763a87ecf694279e67a10313d1d1e130a97fdf
Mobin Afshari <mma137813781378@gmail.com>
2025-09-15 21:41:33 +0330

    feat: working on sign up form

--- src/app/auth/signup/signup.component.ts
+++ src/app/auth/signup/signup.component.ts
@@ -1,9 +1,25 @@
 import { Component } from '@angular/core';
+import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-signup',
   standalone: true,
   templateUrl: './signup.component.html',
   styleUrl: './signup.component.css',
+  imports: [ReactiveFormsModule],
 })
-export class SignupComponent {}
+export class SignupComponent {
+  form = new FormGroup({
+    email: new FormControl('', {
+      validators: [Validators.required, Validators.email],
+    }),
+    password: new FormControl('', { validators: [Validators.required, Validators.minLength(6)] }),
+  });
+
+  onSubmit() {
+    console.log(this.form.value);
+  }
+  onReset() {
+    this.form.reset();
+  }
+}
