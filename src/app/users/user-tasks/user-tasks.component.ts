91780c520cca578df32a49207cf115a8ac0a5f12
Mobin Afshari <mma137813781378@gmail.com>
2025-09-16 23:03:01 +0330

    fix: last changes

--- src/app/users/user-tasks/user-tasks.component.ts
+++ src/app/users/user-tasks/user-tasks.component.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, computed, DestroyRef, inject, input, OnInit } from '@angular/core';
+import { UsersService } from '../users.service';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-user-tasks',
@@ -6,4 +8,19 @@ import { Component } from '@angular/core';
   templateUrl: './user-tasks.component.html',
   styleUrl: './user-tasks.component.css',
 })
-export class UserTasksComponent {}
+export class UserTasksComponent implements OnInit {
+  // userId = input.required<string>();
+  private userService = inject(UsersService);
+  private activatedRoute = inject(ActivatedRoute);
+  userName: string | undefined;
+  private destroyRef = inject(DestroyRef);
+  ngOnInit(): void {
+    const subscription = this.activatedRoute.paramMap.subscribe({
+      next: (val) => {
+        this.userName = this.userService.users.find((u) => u.id === val.get('userId'))?.name;
+      },
+    });
+    this.destroyRef.onDestroy(() => subscription.unsubscribe());
+  }
+  // userName = computed(() => this.userService.users.find((u) => u.id === this.userId())?.name);
+}
