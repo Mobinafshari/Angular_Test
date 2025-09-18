0edbb58b67b01e55c264db65a0ace3ac7f2676a7
Mobin Afshari <mma137813781378@gmail.com>
2025-09-18 20:43:55 +0330

    feat: added fallback for routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,12 +3,18 @@ import { TasksComponent } from './tasks/tasks.component';
 import { NoTaskComponent } from './tasks/no-task/no-task.component';
 import { UserTasksComponent } from './users/user-tasks/user-tasks.component';
 import { NewTaskComponent } from './tasks/new-task/new-task.component';
+import { NotFoundComponent } from './not-found/not-found.component';
 
 export const routes: Routes = [
   {
     path: 'users/:userId',
     component: UserTasksComponent,
     children: [
+      {
+        path: '',
+        redirectTo: 'tasks',
+        pathMatch: 'prefix',
+      },
       {
         path: 'tasks',
         component: TasksComponent,
@@ -22,5 +28,9 @@ export const routes: Routes = [
   {
     path: '',
     component: NoTaskComponent,
+  },
+  {
+    path: '**',
+    component: NotFoundComponent,
   },
 ];
