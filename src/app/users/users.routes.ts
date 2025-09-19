11697344e86facd9c3b80ded857e1522fdd17482
Mobin Afshari <mma137813781378@gmail.com>
2025-09-19 22:02:26 +0330

    fix: added lazy loading to routes

--- src/app/users/users.routes.ts
+++ src/app/users/users.routes.ts
@@ -1,7 +1,25 @@
-import { Routes } from '@angular/router';
-import { resolveUserTasks, TasksComponent } from '../tasks/tasks.component';
+import { ResolveFn, Routes } from '@angular/router';
 import { canLeaveEditPage, NewTaskComponent } from '../tasks/new-task/new-task.component';
 import { resolveTile } from './user-tasks/user-tasks.component';
+import { Task } from '../tasks/task/task.model';
+import { inject } from '@angular/core';
+import { TasksService } from '../tasks/tasks.service';
+
+const resolveUserTasks: ResolveFn<Task[]> = (activatedRouteSnapshot, routerState) => {
+  const order = activatedRouteSnapshot.queryParams['order'];
+  const tasksService = inject(TasksService);
+  const tasks = tasksService
+    .allTasks()
+    .filter((task) => task.userId === activatedRouteSnapshot.paramMap.get('userId'));
+
+  if (order && order === 'asc') {
+    tasks.sort((a, b) => (a.id > b.id ? 1 : -1));
+  } else {
+    tasks.sort((a, b) => (a.id > b.id ? -1 : 1));
+  }
+
+  return tasks.length ? tasks : [];
+};
 
 export const userRoutes: Routes = [
   {
@@ -12,7 +30,7 @@ export const userRoutes: Routes = [
   },
   {
     path: 'tasks',
-    component: TasksComponent,
+    loadComponent: () => import('../tasks/tasks.component').then((mod) => mod.TasksComponent),
     runGuardsAndResolvers: 'paramsOrQueryParamsChange',
     resolve: {
       userTasks: resolveUserTasks,
