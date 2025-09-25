888e63b1d3b9ad1c7d21ab517e7a61cfebfa947d
Mobinafshari <mma137813781378@gmail.com>
2025-09-25 16:56:10 +0330

    feat: working from scratch to test some stuffs

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,15 +1,13 @@
 import { Component } from '@angular/core';
 
-import { CounterComponent } from './counter/counter.component';
-import { MessagesComponent } from './messages/messages.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
-  imports: [CounterComponent, MessagesComponent],
 })
+
 export class AppComponent {
   get debugOutput() {
     console.log('[AppComponent] "debugOutput" binding re-evaluated.');
