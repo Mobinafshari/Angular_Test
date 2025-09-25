e7f1d49f30ce03b22f0efc29181aa3cf83eb49f6
Mobinafshari <mma137813781378@gmail.com>
2025-09-25 17:01:44 +0330

    fix: working from scratch to work basics again

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,19 +1,9 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
-
-import { CounterComponent } from './counter/counter.component';
-import { MessagesComponent } from './messages/messages.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
-  imports: [CounterComponent, MessagesComponent],
 })
-export class AppComponent {
-  get debugOutput() {
-    console.log('[AppComponent] "debugOutput" binding re-evaluated.');
-    return 'AppComponent Component Debug Output';
-  }
-}
+export class AppComponent {}
