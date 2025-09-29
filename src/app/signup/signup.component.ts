5e82d22780f008f7f544bafe9adf2cc15c593f39
Mobinafshari <mma137813781378@gmail.com>
2025-09-29 12:44:22 +0330

    fix: form was built with constructor and type

--- src/app/signup/signup.component.ts
+++ src/app/signup/signup.component.ts
@@ -15,13 +15,30 @@ import { SignUpForm } from './signUp.model';
   styleUrl: './signup.component.scss',
 })
 export class SignupComponent {
-  private formBuilder = inject(FormBuilder);
-  signUpForm = this.formBuilder.group({
-    userName: ['', Validators.required],
-    password: ['', Validators.required, Validators.minLength(6)],
-    email: ['', Validators.required, Validators.email],
-    phoneNumber: [null, Validators.required],
-  });
+  signUpForm: FormGroup;
+  constructor(readonly formBuilder: FormBuilder) {
+    this.signUpForm = formBuilder.group<SignUpForm>({
+      userName: this.formBuilder.control('', {
+        nonNullable: true,
+        validators: Validators.required,
+      }),
+      password: this.formBuilder.control('', {
+        nonNullable: true,
+        validators: Validators.required,
+      }),
+      email: this.formBuilder.control('', { nonNullable: true, validators: Validators.required }),
+      phoneNumber: this.formBuilder.control<number | null>(null, {
+        validators: Validators.required,
+      }),
+    });
+  }
+  // private formBuilder = inject(FormBuilder);
+  // signUpForm = this.formBuilder.group({
+  //   userName: ['', Validators.required],
+  //   password: ['', Validators.required, Validators.minLength(6)],
+  //   email: ['', Validators.required, Validators.email],
+  //   phoneNumber: [null, Validators.required],
+  // });
 
   onSubmit() {
     console.log('===>', this.signUpForm.value);
