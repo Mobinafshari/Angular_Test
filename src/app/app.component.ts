4978825d23fe7042dfb5004c3b934474dd636086
Mobin Afshari <mma137813781378@gmail.com>
2025-10-02 12:02:01 +0330

    feat: ag-grid added

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,7 +1,9 @@
 import { Component } from '@angular/core';
 import { HeaderComponent } from './header/header.component';
 import { RouterOutlet } from '@angular/router';
+import { AllCommunityModule, ModuleRegistry } from 'ag-grid-community';
 
+ModuleRegistry.registerModules([AllCommunityModule]);
 @Component({
   selector: 'app-root',
   standalone: true,
--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,9 +1,11 @@
 import { Component, DestroyRef, inject, OnInit } from '@angular/core';
 import { ActivatedRoute, Router, RouterOutlet } from '@angular/router';
-
+import { AgGridAngular } from 'ag-grid-angular'; // Angular Data Grid Component
+import { ColDef } from 'ag-grid-community';
+import { GridRow } from './home.model';
 @Component({
   selector: 'app-home',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, AgGridAngular],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss',
 })
@@ -20,4 +22,15 @@ export class HomeComponent implements OnInit {
     });
     this.destroyRef.onDestroy(() => sub.unsubscribe());
   }
+  colDefs: ColDef[] = [
+    { field: 'make' },
+    { field: 'model' },
+    { field: 'price' },
+    { field: 'electric' },
+  ];
+  rowData: GridRow[] = [
+    { make: 'Tesla', model: 'Model Y', price: 64950, electric: true },
+    { make: 'Ford', model: 'F-Series', price: 33850, electric: false },
+    { make: 'Toyota', model: 'Corolla', price: 29600, electric: false },
+  ];
 }
