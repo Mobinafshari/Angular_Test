c5c65039756a4f458c29ebe3a8fde5a57eb94bb3
Mobinafshari <mma137813781378@gmail.com>
2025-10-10 17:01:40 +0330

    fix: last changes

---
+++ src/app/shared/select.directive.ts
@@ -0,0 +1,11 @@
+import { Directive, TemplateRef } from '@angular/core';
+
+@Directive({
+  selector: '[appSelectDirective]',
+})
+export class SelectDirective {
+  constructor(
+    private readonly templateRef: TemplateRef,
+    private readonly viewContainerRef: ViewContainerRef
+  ) {}
+}
